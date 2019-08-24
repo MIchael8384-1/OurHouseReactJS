89a5ae89b93baa90268a6738301daa860dcba183
James Clegg <47791084+james-clegg@users.noreply.github.com>
2019-08-24 16:01:34 +0100

    Aframe (#3)
    * npx create-react-app created
    
    * added initial files for aframe, need to sort webpack issue
    
    * aframe will now only render on route /360 rather than overwriting everything
    
    * implemented aframe in main reactjs, including simple routing
    
    * maintenance page
    
    * text area
    
    * finished functionality of maintenance page
    
    * changed component folder name
    
    * Altering index.js

---
+++ ourhousereactjs/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Router } from "@reach/router";
+import Aframe from "./components/Aframe/src/index.js";
+import HomePage from "./components/Homepage/HomePage";
+import MaintenancePage from "./components/Maintenance/MaintenancePage";
+
+class App extends Component {
+  state = {
+    username: 'NA'
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <HomePage path="/" />
+          <Aframe path="/360" userID={this.state.username} />
+          <MaintenancePage path="/maintenance" />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ourhousereactjs/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+// if (window.location.pathname !== "/360") {
+ReactDOM.render(<App />, document.getElementById("root"));
+// } else ReactDOM.render(<Aframe />, document.querySelector("#sceneContainer"));
