03951e8435ffae0d1e5a08fa5c2e63ce0eb0212e
Kate Bryan <katebryan31@gmail.com>
2019-08-29 09:30:50 +0100

    styled in line with branding

--- ourhousereactjs/src/components/Maintenance/MaintenancePage.js
+++ ourhousereactjs/src/components/Maintenance/MaintenancePage.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import CurrentIssueCard from "./CurrentIssueCard";
 import { Link } from "@reach/router";
-import images from './images'
+import images from "./images";
+import styled from "styled-components";
+import "./maintenance.css";
+import "./maintenance-btn.css";
 
-import './maintenance.css'
-import './maintenance-btn.css'
+const H2 = styled.h2`
+  margin-left: 10px;
+`;
 
 class MaintenancePage extends Component {
   state = {
@@ -13,29 +17,30 @@ class MaintenancePage extends Component {
     currentIssues: [
       {
         issue_id: 1,
-        name: "Stain on sofa",
+        name: "Mould behind/ next to radiator",
         location: "1D",
-        desc: "there is a big ass stain on the sofa arm"
+        desc: "large patch of mould in kitchen"
       },
       {
         issue_id: 2,
-        name: "damp on ceiling",
+        name: "stained tiles in utility",
         location: "1F",
-        desc: "damp behind the sofa also"
+        desc: "damaged tiles in utility room"
       }
-    ],
-    
+    ]
   };
 
   render() {
-  return (
-    <div className="maintenance-page">
-      <h1 className="maintenance-title">Maintenance</h1>
-      <p className="maintenance-subtext">Here is a list of your current issues, be sure to message your landlord when you have uploaded a new issue!</p>
-        <h2>Current issues</h2>
-      <div className="maintenance-current-issues">
-        
-          {this.state.currentIssues.map((issue,i) => {
+    return (
+      <div className="maintenance-page">
+        <h1 className="maintenance-title">Maintenance</h1>
+        <p className="maintenance-subtext">
+          Here is a list of your current issues, be sure to message your
+          landlord when you have uploaded a new issue!
+        </p>
+        <H2>Current issues</H2>
+        <div className="maintenance-current-issues">
+          {this.state.currentIssues.map((issue, i) => {
             return (
               <CurrentIssueCard
                 key={issue.issue_id}
@@ -45,20 +50,15 @@ class MaintenancePage extends Component {
               />
             );
           })}
-          
-          
-       
+        </div>
+        <div className="add-item">
+          <Link to="/maintenance/newissue" className="add-item-btn">
+            add item
+          </Link>
+        </div>
       </div>
-      <div className="add-item">
-       
-      <Link to="/maintenance/newissue"className="add-item-btn">add item</Link>
-      </div>
-    
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default MaintenancePage;
-
-
